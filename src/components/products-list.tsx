8d77ef35fbbde85e3fdcf290d2abef97b63c2505
Guilherme Marques <guimarquesjob@gmail.com>
2024-01-10 11:11:46 -0300

    fix: creating notFound, no search and loading cards

--- src/components/products-list.tsx
+++ src/components/products-list.tsx
@@ -20,7 +20,36 @@ const NoSearchContainer = styled.div`
   justify-content: center;
   text-align: center;
   gap: 24px;
-  
+
+  h1 {
+    font-size: 32px;
+    color: var(--primary-button);
+  }
+
+  h3 {
+    font-size: 20px;
+    color: var(--secondary-text);
+  }
+`;
+
+const IsLoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 32px;
+  color: var(--primary-button);
+  font-weight: 700;
+`;
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: 24px;
+
   h1 {
     font-size: 32px;
     color: var(--primary-button);
@@ -34,11 +63,19 @@ const NoSearchContainer = styled.div`
 
 function ProductsList() {
   const { query, selectedCategory } = useContext(StoreContext);
-  const { products } = useProducts(selectedCategory, query);
-
+  const { products, isLoading } = useProducts(selectedCategory, query);
+  console.log(products);
+  
   return (
     <ListContainer>
-      {selectedCategory.id === 0 ? (
+      {(!isLoading && products?.length === 0 && query !== '') && (
+        <NotFoundContainer>
+          <h1>NENHUM PRODUTO FOI ENCONTRADO</h1>
+          <h3>Digite outro termo de pesquisa ou escolha uma categoria</h3>
+        </NotFoundContainer>
+      )}
+      {isLoading && <IsLoadingContainer>Carregando...</IsLoadingContainer>}
+      {selectedCategory.id === 0 && query === "" ? (
         <NoSearchContainer>
           <h1>VOCÊ AINDA NÃO REALIZOU UMA BUSCA</h1>
           <h3>Digite algum termo de pesquisa ou escolha uma categoria</h3>
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -5,12 +5,17 @@ import { useQuery } from "@tanstack/react-query";
 export const useProducts = (selectedCategory: CategoryType, query: string) => {
 
   const getProducts = async () => {
-    const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${selectedCategory.id}&q=${query}`);
+    let result;
+    if (selectedCategory.id === 0 && query !== '') {
+      result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    } else {
+      result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${selectedCategory.id}&q=${query}`);
+    }
     const data = await result.json();
     return data;
   }
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['products', selectedCategory, query],
     queryFn: () => getProducts(),
     staleTime: 1000 * 60 * 60 * 24,
@@ -18,5 +23,6 @@ export const useProducts = (selectedCategory: CategoryType, query: string) => {
 
   return {
     products: data?.results,
+    isLoading,
   }
 }
